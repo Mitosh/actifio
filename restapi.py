7aff4ee62e4423d48705b4e14f5b74669f7a9ffd
Mitosh <rouben.sefilian@gmail.com>
2022-05-13 10:56:23 +0200

    t

--- restapi.py
+++ restapi.py
@@ -9,24 +9,26 @@ def get_session_id():
     headers1 =  {"Authorization" : "Basic YWRtaW46T3JhbmdlMTIzIQ=="}
     response = requests.post(api_url, headers=headers1, verify=False)
     json_test= response.json()
-    return json_test["sessionid"]
+    return json_test
 
+
+#return json_test["sessionid"]
 #call the function to get seesion id and save it in sessionid variable
 sessionid = get_session_id()
 #print the session id
 print(sessionid)
 
 
-api_url1 = "https://34.116.223.174/actifio/api/info/lshost"
-headers2 =  {"Authorization" : "Actifio "+ sessionid }
-response1 = requests.get(api_url1, headers=headers2, verify=False)
+#api_url1 = "https://34.116.223.174/actifio/api/info/lshost"
+#headers2 =  {"Authorization" : "Actifio "+ sessionid }
+#response1 = requests.get(api_url1, headers=headers2, verify=False)
 #print(response1.text)
 
 #format the json
-json_object = json.loads(response1.text)
-json_formatted_str = json.dumps(json_object, indent=2)
+#json_object = json.loads(response1.text)
+#json_formatted_str = json.dumps(json_object, indent=2)
 #print the json 
-print(json_formatted_str)
+#print(json_formatted_str)
 
 #response = requests.get(api_url, verify=False)
 #print(response.text)
