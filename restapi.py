a00d60f69365afb42049ef82db5a63365d68a714
Mitosh <rouben.sefilian@gmail.com>
2022-05-13 11:05:37 +0200

    t

--- restapi.py
+++ restapi.py
@@ -22,7 +22,7 @@ print(sessionid)
 
 def get_application():
     api_url ="https://10.30.10.3/actifio/application?sort=appname%3Aasc&filter=hostname%3A%3D%7Cmyhost&limit=50&offset=0"
-    headers = {"Authorization" : "actifio " + sessionid}
+    headers = {"Authorization" : "actifio b35bcc57-1245-4feb-85fe-b3b6991dc905" }
     response = requests.post(api_url, headers=headers, verify=False)
     get_test= response.json()
     return get_test
