12888cb0d742e9ab7e60a5f95454c32fe2f25e8e
Mitosh <rouben.sefilian@gmail.com>
2022-05-13 11:04:20 +0200

    t

--- restapi.py
+++ restapi.py
@@ -12,34 +12,20 @@ def get_session_id():
     return json_test["session_id"]
 
 
-#return json_test["sessionid"]
+
 #call the function to get seesion id and save it in sessionid variable
 sessionid = get_session_id()
 #print the session id
 print(sessionid)
 
 
-#api_url1 = "https://34.116.223.174/actifio/api/info/lshost"
-#headers2 =  {"Authorization" : "Actifio "+ sessionid }
-#response1 = requests.get(api_url1, headers=headers2, verify=False)
-#print(response1.text)
 
-#format the json
-#json_object = json.loads(response1.text)
-#json_formatted_str = json.dumps(json_object, indent=2)
-#print the json 
-#print(json_formatted_str)
+def get_application():
+    api_url ="https://10.30.10.3/actifio/application?sort=appname%3Aasc&filter=hostname%3A%3D%7Cmyhost&limit=50&offset=0"
+    headers = {"Authorization" : "actifio " + sessionid}
+    response = requests.post(api_url, headers=headers, verify=False)
+    get_test= response.json()
+    return get_test
 
-#response = requests.get(api_url, verify=False)
-#print(response.text)
-
-##load the resoult of the get into json 
-
-#format the json
-#json_formatted_str = json.dumps(json_object, indent=2)
-#print the json 
-#print(json_formatted_str)
-
-#json_test= response.json()
-
-#print(json_test["sessionid"])
+get_apl = get_application()
+print(get_apl)
